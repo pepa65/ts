db0438b7ad57ffd5d780db5d2b73d5f4bf8e4cc2
pepa65 <pepa65@passchier.net>
2022-04-01 12:56:27 +0700

    Try struct members

--- ts.go
+++ ts.go
@@ -9,28 +9,8 @@ package ts
 
 // Return System Size
 type Size struct {
-	row  uint16
-	col  uint16
-	posX uint16
-	posY uint16
-}
-
-// Get Terminal Width
-func (w Size) Col() int {
-	return int(w.col)
-}
-
-// Get Terminal Height
-func (w Size) Row() int {
-	return int(w.row)
-}
-
-// Get Position X
-func (w Size) PosX() int {
-	return int(w.posX)
-}
-
-// Get Position Y
-func (w Size) PosY() int {
-	return int(w.posY)
+	Row uint16
+	Col uint16
+	X   uint16
+	Y   uint16
 }
